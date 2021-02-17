06c31b3e933e5d79a7466f7fd280f60fe0fc9e77
ale-cci <alecrd98@gmail.com>
2021-02-17 21:25:36 +0100

    Fix migration syntax

--- migrations/004-add-application-tables.sql
+++ migrations/004-add-application-tables.sql
@@ -1,7 +1,7 @@
 create table oauth_apps (
-    id integer primary key auto_increment;
-    appname varchar(256) not null;
-    redirect_uri varchar(2048) not null;
-    client_id varchar(256) not null;
-    client_secret varchar(1024) not null;
+    id integer primary key auto_increment,
+    appname varchar(256) not null,
+    redirect_uri varchar(2048) not null,
+    client_id varchar(256) not null,
+    client_secret varchar(1024) not null
 );
